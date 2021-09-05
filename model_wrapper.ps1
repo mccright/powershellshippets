e928b39c6843bcfb6100edb43e6f2a0d90175eb5
mccright <6339517+mccright@users.noreply.github.com>
2021-09-05 17:59:07 -0500

    initial commit model_wrapper.ps1

---
+++ model_wrapper.ps1
@@ -0,0 +1,25 @@
+# This model is only for scripts that you write and understand
+# First, ensure that the PowerShell Execution Policy is set to 'ByPass'
+$registryPath = "HKLM:\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell"
+$Name = "ExecutionPolicy"
+$valueString = "ByPass"
+$execPol = (Get-ItemProperty -Path $registryPath -Name $Name).$Name
+
+if(!($execPol -eq $valueString))  { 
+    New-ItemProperty -Path $registryPath -Name $name -Value $valueString -PropertyType String -Force | Out-Null
+    }
+
+# Now do whatever scripting that we need to do
+# ...
+# ...
+# ...
+
+# After completing everything we needed to do, 
+# set the PowerShell Execution Policy back to 'RemoteSigned'
+$valueString = "RemoteSigned"
+$execPol = (Get-ItemProperty -Path $registryPath -Name $Name).$Name
+
+if(!($execPol -eq $valueString))  { 
+    New-ItemProperty -Path $registryPath -Name $name -Value $valueString -PropertyType String -Force | Out-Null
+    }
+
